c546c85c9ef623ef39c4c8d6d6a3977b03e6c846
Marie Ramlow <me@nycode.dev>
2023-04-29 06:03:40 +0200

    this does not work lol

--- catcafe.nix
+++ catcafe.nix
@@ -109,4 +109,19 @@
     alsa.support32Bit = true;
     pulse.enable = true;
   };
+
+  nixpkgs.config.packageOverrides = pkgs: {
+    vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
+  };
+
+  hardware.opengl = {
+    enable = true;
+    extraPackages = with pkgs; [
+      # intel-media-driver # LIBVA_DRIVER_NAME=iHD
+      vaapiIntel         # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+      vaapiVdpau
+      libvdpau-va-gl
+    ];
+    driSupport32Bit = true;
+  };
 }
