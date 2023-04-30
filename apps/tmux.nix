177a00918ab73584b62bfab029fa5daebe63977f
Marie Ramlow <me@nycode.dev>
2023-04-30 18:19:30 +0200

    Configure tmux

---
+++ apps/tmux.nix
@@ -0,0 +1,19 @@
+{ pkgs
+, config
+, lib
+, ...
+}:
+
+{
+  programs.tmux = {
+    enable = true;
+    keyMode = "vi";
+    mouse = true;
+    sensibleOnTop = true;
+    clock24 = true;
+    terminal = "screen-256color";
+    extraConfig = ''
+      set -ga terminal-overrides ",xterm-256color*:RGB"
+    '';
+  };
+}
--- packages/default.nix
+++ packages/default.nix
@@ -45,5 +45,7 @@
     pulseaudio
     pipewire
     wireplumber
+
+    tmux
   ];
 }
