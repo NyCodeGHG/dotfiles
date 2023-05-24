0ff87bc87d748b6235f8bb48889d4e0a7044b133
Marie Ramlow <me@nycode.dev>
2023-05-24 17:05:15 +0200

    Revert "Use rtl88x2bu driver"
    This reverts commit 3855a03ca52d615d0de3b6699b0f4cd571aeec43.

--- hosts/catcafe/default.nix
+++ hosts/catcafe/default.nix
@@ -25,11 +25,6 @@
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
-  boot.kernelModules = [ "88x2bu" ];
-  boot.extraModulePackages = [
-    config.boot.kernelPackages.rtl88x2bu
-  ];
-
   networking.hostName = "catcafe"; # Define your hostname.
   networking.networkmanager = {
     enable = true;
