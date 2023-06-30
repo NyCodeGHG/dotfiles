78b60ebf25372b247efe777014f7083fda067671
Marie Ramlow <me@nycode.dev>
2023-06-30 16:46:20 +0200

    Configure ipv6

--- hosts/artemis/default.nix
+++ hosts/artemis/default.nix
@@ -32,6 +32,18 @@
       '';
     };
     nftables.enable = true;
+    interfaces = {
+      ens3.ipv6.addresses = [
+        {
+          address = "2a03:4000:5f:f5b::";
+          prefixLength = 64;
+        }
+      ];
+    };
+    defaultGateway6 = {
+      address = "fe80::1";
+      interface = "ens3";
+    };
   };
 
   time.timeZone = "Europe/Berlin";
