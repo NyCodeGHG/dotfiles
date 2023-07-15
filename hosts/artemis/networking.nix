22de638322db43bd0da7601029f2d777b52929bb
Marie Ramlow <me@nycode.dev>
2023-07-15 15:13:30 +0200

    add another ipv6

--- hosts/artemis/networking.nix
+++ hosts/artemis/networking.nix
@@ -13,6 +13,10 @@
           address = "2a03:4000:5f:f5b::";
           prefixLength = 64;
         }
+        {
+          address = "2a03:4000:005f:0f5b:b00b:1337:cafe:4269";
+          prefixLength = 128;
+        }
       ];
     };
     defaultGateway6 = {
