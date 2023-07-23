62833df2c4c74e4d3f4514a6c78a0e9bac53f59c
Marie Ramlow <me@nycode.dev>
2023-07-23 08:36:08 +0200

    improve ssh profile

--- home/profiles/ssh.nix
+++ home/profiles/ssh.nix
@@ -6,15 +6,37 @@
     matchBlocks = {
       "github.com" = {
         user = "git";
+        identitiesOnly = true;
+        identityFile = "~/.ssh/github.ed25519";
+      };
+      catcafe = {
+        hostname = "192.168.1.35";
+        identityFile = "~/.ssh/github.ed25519";
       };
       artemis = {
         hostname = "nue01.marie.cologne";
+        identitiesOnly = true;
+        identityFile = "~/.ssh/default.ed25519";
       };
       delphi = {
         hostname = "oci-fra01.marie.cologne";
+        identitiesOnly = true;
+        identityFile = "~/.ssh/default.ed25519";
       };
       raspberrypi = {
         user = "pi";
+        identityFile = "~/.ssh/default.ed25519";
+        identitiesOnly = true;
+      };
+      nas = {
+        hostname = "10.69.0.8";
+        identityFile = "~/.ssh/default.ed25519";
+        identitiesOnly = true;
+      };
+      "*" ={
+        extraOptions = {
+          AddKeysToAgent = "yes";
+        };
       };
     };
   };
