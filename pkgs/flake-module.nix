d588bc303c376b925e17305ea173810f4c6ea162
Marie Ramlow <me@nycode.dev>
2023-12-17 19:46:08 +0100

    remove tf2-server package

--- pkgs/flake-module.nix
+++ pkgs/flake-module.nix
@@ -3,10 +3,6 @@
     packages = {
       node-mixin = pkgs.callPackage ./node-mixin { };
       inherit (pkgs.callPackage ./renovate { }) renovate;
-#      tf2-server = pkgs.callPackage ./tf2-server { };
-#      tf2-server-wrapped = pkgs.callPackage ./tf2-server/fhsenv.nix {
-#        tf2-server-unwrapped = self'.packages.tf2-server;
-#      };
       # preview-colors = pkgs.callPackage ./preview-colors { };
       guard-nvim = pkgs.callPackage ./guard-nvim { };
       deploy = pkgs.callPackage ../tools/deploy { };
