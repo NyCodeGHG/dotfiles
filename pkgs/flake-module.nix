deaa71ff0b83d8b02c0e1501aa2e990ae720e8ec
Marie Ramlow <me@nycode.dev>
2024-06-09 17:23:09 +0200

    get rid of node2nix renovate

--- pkgs/flake-module.nix
+++ pkgs/flake-module.nix
@@ -2,7 +2,6 @@
   perSystem = { pkgs, config, inputs', self', ... }: {
     packages = {
       node-mixin = pkgs.callPackage ./node-mixin { };
-      inherit (pkgs.callPackage ./renovate { }) renovate;
       # preview-colors = pkgs.callPackage ./preview-colors { };
       guard-nvim = pkgs.callPackage ./guard-nvim { };
       deploy = pkgs.callPackage ../tools/deploy { };
