b512b6cfb7475932acda05d21db709a22996f165
Marie Ramlow <me@nycode.dev>
2024-06-20 18:27:32 +0200

    add new ssh key

--- config/nixos/users/marie.nix
+++ config/nixos/users/marie.nix
@@ -5,8 +5,10 @@ lib.mkIf config.uwumarie.profiles.users.marie {
     isNormalUser = true;
     extraGroups = [ "wheel" ];
     openssh.authorizedKeys.keys = lib.mkIf config.services.openssh.enable [
-      # Desktop
+      # Desktop old
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFiS+tzh0R/nN5nqSwvLerCV4nBwI51zOKahFfiiINGp"
+      # Desktop
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILdNaJgKxA021pqrbkoMiP2a9buYZUXfG5q01y2h8YOa"
       # Laptop
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIESHraJJ0INX/OAXOQUR4UuLEre/2N70Uh3H5YkFC5zz"
     ];
