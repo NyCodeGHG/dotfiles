0c6afc66c033a52b6c3913bfd1fab5b231f3acc3
Marie Ramlow <me@nycode.dev>
2024-08-12 15:53:48 +0200

    marie-desktop: allow steam lan features

--- hosts/marie-desktop/gaming.nix
+++ hosts/marie-desktop/gaming.nix
@@ -26,6 +26,8 @@
     extraCompatPackages = with pkgs; [
       proton-ge-bin
     ];
+    localNetworkGameTransfers.openFirewall = true;
+    remotePlay.openFirewall = true;
   };
   # Fixes Hogwarts Legacy to not crash
   boot.kernel.sysctl."vm.max_map_count" = 2146483642;
