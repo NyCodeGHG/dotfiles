07e98ddb09d8e26160027d26f37042646601c963
Marie Ramlow <me@nycode.dev>
2024-09-05 20:08:48 +0200

    nixosConfigurations.marie-desktop.config.hardware.nvidia.package: 555.58.02 -> 560.35.03

--- hosts/marie-desktop/nvidia.nix
+++ hosts/marie-desktop/nvidia.nix
@@ -38,8 +38,8 @@
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
     # package = config.boot.kernelPackages.nvidiaPackages.production;
     package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-      version = "555.58.02";
-      sha256_64bit = "sha256-xctt4TPRlOJ6r5S54h5W6PT6/3Zy2R4ASNFPu8TSHKM=";
+      version = "560.35.03";
+      sha256_64bit = "sha256-8pMskvrdQ8WyNBvkU/xPc/CtcYXCa7ekP73oGuKfH+M=";
       sha256_aarch64 = "";
       openSha256 = "";
       settingsSha256 = "sha256-ZpuVZybW6CFN/gz9rx+UJvQ715FZnAOYfHn5jt5Z2C8=";
