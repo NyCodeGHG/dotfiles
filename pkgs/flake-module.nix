a6b47e494b3450e5fc61054c9e9f3445bd963168
Marie Ramlow <me@nycode.dev>
2024-09-19 17:53:19 +0200

    node-mixin: drop

--- pkgs/flake-module.nix
+++ pkgs/flake-module.nix
@@ -1,7 +1,6 @@
 {
   perSystem = { pkgs, config, inputs', self', ... }: {
     packages = {
-      node-mixin = pkgs.callPackage ./node-mixin { };
       # preview-colors = pkgs.callPackage ./preview-colors { };
       guard-nvim = pkgs.callPackage ./guard-nvim { };
       sandwine = pkgs.callPackage ./sandwine { };
