2d6480e767e92d957ceab5a3ea7d30993dbf9355
Marie Ramlow <me@nycode.dev>
2024-09-19 18:24:37 +0200

    add qpm-cli, wgsl-analyzer to packages output

--- pkgs/flake-module.nix
+++ pkgs/flake-module.nix
@@ -2,6 +2,8 @@
   perSystem = { pkgs, config, inputs', self', ... }: {
     packages = {
       sandwine = pkgs.callPackage ./sandwine { };
+      qpm-cli = pkgs.callPackage ./qpm-cli { };
+      wgsl-analyzer = pkgs.callPackage ./wgsl-analyzer/package.nix { };
     };
   };
 }
