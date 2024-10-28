4974234a70d983e19ad255598f507b8ad3b51b06
Marie Ramlow <me@nycode.dev>
2024-10-28 19:54:06 +0100

    nginx: add simple website, so it doesn't 404

--- hosts/artemis/applications/nginx-meta.nix
+++ hosts/artemis/applications/nginx-meta.nix
@@ -1,13 +1,39 @@
 { ... }:
 {
-  services.nginx.virtualHosts."marie.cologne" = {
-    locations."/" = {
-      tryFiles = "$uri =404";
-      root = "/var/lib/www";
-      extraConfig = ''
-        add_header 'Content-Security-Policy' 'upgrade-insecure-requests';
-      '';
-      index = "index.html";
+  services.nginx.virtualHosts = {
+    "marie.cologne" = {
+      locations."/" = {
+        root = "${../website}";
+        index = "index.html";
+        tryFiles = "$uri $uri/index.html @fallback";
+        extraConfig = ''
+          add_header 'Content-Security-Policy' 'upgrade-insecure-requests';
+        '';
+      };
+      locations."@fallback" = {
+        root = "/var/lib/www";
+        tryFiles = "$uri =404";
+        extraConfig = ''
+          add_header 'Content-Security-Policy' 'upgrade-insecure-requests';
+        '';
+      };
+    };
+    "marie.dn42" = {
+      locations."/" = {
+        root = "${../website}";
+        index = "index.html";
+        tryFiles = "$uri $uri/index.html @fallback";
+        extraConfig = ''
+          add_header 'Content-Security-Policy' 'upgrade-insecure-requests';
+        '';
+      };
+      locations."@fallback" = {
+        root = "/var/lib/www";
+        tryFiles = "$uri =404";
+        extraConfig = ''
+          add_header 'Content-Security-Policy' 'upgrade-insecure-requests';
+        '';
+      };
     };
   };
 }
