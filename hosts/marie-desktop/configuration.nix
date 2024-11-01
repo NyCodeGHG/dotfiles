0b3df5ec886b5b3c86218957b1b29afcdbd8774c
Marie Ramlow <me@nycode.dev>
2024-11-01 15:33:57 +0100

    marie-desktop(sddm): enable wayland with kwin

--- hosts/marie-desktop/configuration.nix
+++ hosts/marie-desktop/configuration.nix
@@ -45,7 +45,13 @@
     font = "Lat2-Terminus16";
   };
   services = {
-    displayManager.sddm.enable = true;
+    displayManager.sddm = {
+      enable = true;
+      wayland = {
+        enable = true;
+        compositor = "kwin";
+      };
+    };
     desktopManager.plasma6.enable = true;
     xserver.enable = true;
     xserver.xkb.layout = "de";
