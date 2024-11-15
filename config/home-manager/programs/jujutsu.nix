ae18da3451c0a83d0520a58f8ca84df3c313204a
Marie Ramlow <me@nycode.dev>
2024-11-15 21:50:34 +0100

    jujutsu: add roots alias

--- config/home-manager/programs/jujutsu.nix
+++ config/home-manager/programs/jujutsu.nix
@@ -14,6 +14,7 @@ lib.mkIf config.uwumarie.profiles.jujutsu
         gf = [ "git" "fetch" ];
         f-master = [ "git" "fetch" "--branch=master" "--remote=upstream" ];
         bump = [ "bookmark" "move" "--from" "heads(::@- & bookmarks())" "--to" "@-" ];
+        roots = [ "log" "-r" "mine() & visible_heads()" ];
       };
       revsets.log = "trunk() | reachable(@, trunk()..visible_heads())";
     };
