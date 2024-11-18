25b0d171748a906f7958314f2bb1c04cffa30755
Marie Ramlow <me@nycode.dev>
2024-11-18 15:55:24 +0100

    delphi(minecraft): switch modpack to fear-nightfall

--- hosts/delphi/minecraft.nix
+++ hosts/delphi/minecraft.nix
@@ -1,7 +1,7 @@
 { config, inputs, ... }:
 {
   virtualisation.oci-containers.containers.minecraft = {
-    image = "docker.io/itzg/minecraft-server:java8";
+    image = "docker.io/itzg/minecraft-server:java21";
     environment = {
       EULA = "true";
       INIT_MEMORY = "4G";
@@ -9,28 +9,31 @@
       ENABLE_ROLLING_LOGS = "true";
       TZ = "Europe/Berlin";
       ENABLE_WHITELIST = "true";
+      WHITELIST = "uwumarie,techtoto,emmabtw";
+      OPS = "uwumarie";
       USE_NATIVE_TRANSPORT = "true";
-      VIEW_DISTANCE = "20";
+      VIEW_DISTANCE = "16";
       SPAWN_PROTECTION = "0";
       ALLOW_FLIGHT = "true";
-      TYPE = "CUSTOM";
-      CUSTOM_SERVER = "forge-1.12.2-14.23.5.2860.jar";
-      GENERIC_PACK = "https://github.com/Nomi-CEu/Nomi-CEu/releases/download/1.7-alpha-4/nomi-ceu-1.7-alpha-4-server.zip";
+      TYPE = "AUTO_CURSEFORGE";
+      CF_SLUG = "fear-nightfall";
+      CF_FILENAME_MATCHER = "Fear Nightfall Remains of Chaos-v1.0.6.zip";
+      CF_EXCLUDE_MODS = "yungs-menu-tweaks";
       USE_AIKAR_FLAGS = "true";
       UID = toString config.users.users.minecraft.uid;
       GID = toString config.users.groups.minecraft.gid;
     };
     environmentFiles = [ config.age.secrets.curseforge-api-key.path ];
     volumes = [
-      "/var/lib/minecraft/nomi-ceu:/data"
+      "/var/lib/minecraft/fear-nightfall:/data"
     ];
-    extraOptions = [
-      "--network=host"
+    ports = [
+      "25565:25565"
+      "24454:24454/udp"
     ];
     user = "${toString config.users.users.minecraft.uid}:${toString config.users.groups.minecraft.gid}";
   };
-  networking.firewall.allowedTCPPorts = [ 25565 ];
-  systemd.tmpfiles.settings."10-minecraft"."/var/lib/minecraft/nomi-ceu".d = {
+  systemd.tmpfiles.settings."10-minecraft"."/var/lib/minecraft/fear-nightfall".d = {
     group = "minecraft";
     mode = "0775";
     user = "minecraft";
@@ -46,6 +49,18 @@
       gid = 982;
     };
   };
+  networking.firewall = {
+    # open minecraft port
+    allowedTCPPorts = [ 25565 ];
+    allowedUDPPorts = [ 24454 ];
+  };
   age.secrets.curseforge-api-key.file = "${inputs.self}/secrets/curseforge-api-key.age";
-  virtualisation.podman.defaultNetwork.settings.ipv6_enabled = true;
+  virtualisation.oci-containers.backend = "podman";
+  virtualisation.podman = {
+    enable = true;
+    defaultNetwork.settings = {
+      dns_enabled = true;
+    };
+  };
+  users.users.marie.extraGroups = [ "minecraft" ];
 }
