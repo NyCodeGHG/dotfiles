bf091119dd7c8487f2293b19ea51b0d680b1f85f
Marie Ramlow <me@nycode.dev>
2024-11-23 18:20:12 +0100

    suspend fix the second

--- hosts/marie-desktop/suspend-fix.nix
+++ hosts/marie-desktop/suspend-fix.nix
@@ -3,14 +3,19 @@
   systemd.services.fix-suspend = {
     description = "Fix suspend crash";
     unitConfig.Type = "oneshot";
+    before = [
+      "systemd-suspend.service"
+      "systemd-hibernate.service"
+      "systemd-hybrid-sleep.service"
+      "systemd-suspend-then-hibernate.service"
+    ];
     serviceConfig = {
       ExecStart = lib.concatStringsSep " " [
         (lib.getExe pkgs.bash)
         "-c"
-        (lib.escapeShellArg "echo GPP0 > /proc/acpi/wakeup")
+        (lib.escapeShellArg "echo GPP0 > /proc/acpi/wakeup && echo GPP8 > /proc/acpi/wakeup")
       ];
-      RemainAfterExit = true;
     };
-    wantedBy = [ "multi-user.target" ];
+    wantedBy = [ "sleep.target" "multi-user.target" ];
   };
 }
