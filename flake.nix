a5d160b1770b0ee68a097ce738088edae150a065
Marie Ramlow <me@nycode.dev>
2025-01-01 19:42:20 +0100

    flake: switch to unstable-small

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable-small";
 
     home-manager-unstable = {
       url = "github:nix-community/home-manager";
