a7d600d431e846c5e047fcd35749322b79d50a49
Marie Ramlow <me@nycode.dev>
2025-01-08 19:13:35 +0100

    uptime-kuma: migrate to tailscale vpn

--- hosts/artemis/monitoring/uptime-kuma.nix
+++ hosts/artemis/monitoring/uptime-kuma.nix
@@ -4,16 +4,15 @@
     enable = true;
     appriseSupport = true;
   };
+  services.nginx.tailscaleAuth = {
+    enable = true;
+    virtualHosts = [ "uptime-kuma.marie.cologne" ];
+  };
   services.nginx.virtualHosts = {
     "uptime-kuma.marie.cologne" = {
       locations."/" = {
         proxyPass = "http://127.0.0.1:3001";
         proxyWebsockets = true;
-        extraConfig = ''
-          allow 127.0.0.1/24;
-          allow 10.69.0.1/24;
-          deny all;
-        '';
       };
     };
     "status.marie.cologne" = {
