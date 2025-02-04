a4ed1b9bd5031485bfa86eb0854d78b69e2a16ff
Marie Ramlow <me@nycode.dev>
2025-02-04 18:33:18 +0100

    config/nixos/nix: add cache.marie.cologne

--- config/nixos/applications/nix.nix
+++ config/nixos/applications/nix.nix
@@ -12,8 +12,14 @@
         options = "--delete-older-than 14d";
       };
       settings = {
-        substituters = [ "https://uwumarie.cachix.org" ];
-        trusted-public-keys = [ "uwumarie.cachix.org-1:H6nX8e82pu2GQ8CGU3j1qHTG7QMYzZ15oSBh26XhtVo=" ];
+        substituters = [ 
+          "https://cache.marie.cologne/marie"
+          "https://uwumarie.cachix.org"
+        ];
+        trusted-public-keys = [ 
+          "marie:+lOdb5mtdCgfeh1P+Wsk/7okVYuvlv9eOSyihL6rwPs="
+          "uwumarie.cachix.org-1:H6nX8e82pu2GQ8CGU3j1qHTG7QMYzZ15oSBh26XhtVo="
+        ];
         experimental-features = [ "nix-command" "flakes" ];
         trusted-users = [ "@wheel" ];
       };
