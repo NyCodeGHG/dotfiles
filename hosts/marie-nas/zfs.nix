46e10455640617fac6a478bbfbccb414ea381e6a
Marie Ramlow <me@nycode.dev>
2025-02-18 22:52:30 +0100

    marie-nas: setup hd-idle

--- hosts/marie-nas/zfs.nix
+++ hosts/marie-nas/zfs.nix
@@ -1,8 +1,21 @@
-{ ... }:
+{ pkgs, lib, ... }:
 {
   networking.hostId = "5b001dfe";
   boot = {
     supportedFilesystems.zfs = true;
     zfs.forceImportRoot = false;
+  };
+
+  systemd.services.hd-idle = {
+    description = "Spin down disks";
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "${lib.getExe pkgs.hd-idle} -i 1200";
+      DynamicUser = true;
+      SupplementaryGroups = [ "disk" ];
+      ProtectSystem = "strict";
+      ProtectHome = true;
+    };
   };
 }
