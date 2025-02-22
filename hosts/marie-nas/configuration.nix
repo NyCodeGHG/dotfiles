bef1648c7d7504fed451b94decd39ff5faecbecc
Marie Ramlow <me@nycode.dev>
2025-02-22 18:49:34 +0100

    marie-nas: enable fwupd

--- hosts/marie-nas/configuration.nix
+++ hosts/marie-nas/configuration.nix
@@ -32,6 +32,8 @@
     nginx = true;
   };
 
+  services.fwupd.enable = true;
+
   users.users.marie.openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAs0W2PBnnSG7LvyE0TnfnFjzaC4tbRludscIZM+SWci" ];
 
   environment.systemPackages = with pkgs; [
