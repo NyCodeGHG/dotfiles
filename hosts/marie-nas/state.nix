4425b4fef25242e6ad40a1a979702f4a4f772067
Marie Ramlow <me@nycode.dev>
2025-03-04 13:54:30 +0100

    marie-nas/state: add initrd ssh keys

--- hosts/marie-nas/state.nix
+++ hosts/marie-nas/state.nix
@@ -31,6 +31,8 @@
           { file = "/etc/ssh/ssh_host_rsa_key"; mode = "0700"; inInitrd = true; configureParent = true; how = "symlink"; }
           { file = "/etc/ssh/ssh_host_rsa_key.pub"; inInitrd = true; configureParent = true; how = "symlink"; }
           { file = "/etc/zfs/zpool.cache"; inInitrd = true; configureParent = true; how = "symlink"; }
+          { file = "/etc/secrets/initrd/ssh_host_rsa_key"; inInitrd = true; configureParent = true; how = "symlink"; }
+          { file = "/etc/secrets/initrd/ssh_host_ed25519_key"; inInitrd = true; configureParent = true; how = "symlink"; }
         ];
       };
     };
