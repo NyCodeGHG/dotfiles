5ee236865f5a8b56f6ccd4a3ec2bffa6685f16b3
Marie Ramlow <me@nycode.dev>
2025-03-05 19:37:04 +0100

    marie-nas: fix machine-id persisting

--- hosts/marie-nas/state.nix
+++ hosts/marie-nas/state.nix
@@ -23,8 +23,8 @@
           "/var/lib/acme"
         ];
         files = [
-          { file = "/etc/machine-id"; inInitrd = true; how = "symlink"; }
-          { file = "/etc/NIXOS"; inInitrd = true; how = "symlink"; }
+          { file = "/etc/machine-id"; inInitrd = true; }
+          { file = "/etc/NIXOS"; inInitrd = true; }
           { file = "/etc/ssh/ssh_host_ed25519_key"; mode = "0700"; inInitrd = true; configureParent = true; how = "symlink"; }
           { file = "/etc/ssh/ssh_host_ed25519_key.pub"; inInitrd = true; configureParent = true; how = "symlink"; }
           { file = "/etc/ssh/ssh_host_rsa_key"; mode = "0700"; inInitrd = true; configureParent = true; how = "symlink"; }
@@ -32,13 +32,6 @@
           { file = "/etc/zfs/zpool.cache"; inInitrd = true; configureParent = true; how = "symlink"; }
         ];
       };
-    };
-
-    systemd.suppressedSystemUnits = [ "systemd-machine-id-commit.service" ];
-
-    systemd.services.systemd-machine-id-commit = {
-      unitConfig.ConditionPathIsMountPoint = [ "" "/state/etc/machine-id" ];
-      serviceConfig.ExecStart = [ "" "systemd-machine-id-setup --commit --root /state" ];
     };
 
     boot.initrd.systemd = {
