26b5713cb607c480d47f4a383ae2a211ea32f2b6
Marie Ramlow <me@nycode.dev>
2025-03-25 15:36:00 +0100

    artemis/sso: add unavailable page

--- hosts/artemis/applications/authentik.nix
+++ hosts/artemis/applications/authentik.nix
@@ -15,4 +15,19 @@
     };
   };
   age.secrets.authentik-secrets.file = "${inputs.self}/secrets/authentik-secrets.age";
+
+  services.nginx.virtualHosts."sso.nycode.dev" = {
+    extraConfig = ''
+      error_page 401 /sso_unavailable.html;
+
+      location = /sso_unavailable.html {
+        root ${./nginx};
+        internal;
+      }
+    '';
+  };
+  services.nginx.tailscaleAuth = {
+    enable = true;
+    virtualHosts = [ "sso.nycode.dev" ];
+  };
 }
