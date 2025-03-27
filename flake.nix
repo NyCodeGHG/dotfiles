14884c7bb82b06ef41c3bd1d29332526517d52fe
Marie Ramlow <me@nycode.dev>
2025-03-27 12:41:20 +0100

    flake: nixos-24.11 -> nixos-24.11-small

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11-small";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable-small";
 
     home-manager-unstable = {
