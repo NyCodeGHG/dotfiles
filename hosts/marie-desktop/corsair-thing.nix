2609d36f1ba48409b83c1c9da0c98bdaa3595cc9
Marie Ramlow <me@nycode.dev>
2025-04-04 16:23:50 +0200

    marie-desktop/corsair-thing: run as user service

--- hosts/marie-desktop/corsair-thing.nix
+++ hosts/marie-desktop/corsair-thing.nix
@@ -5,17 +5,13 @@
 
   # corsair headset userspace access
   services.udev.extraRules = ''
-    SUBSYSTEM=="hidraw", ATTRS{idVendor}=="1b1c", ATTRS{idProduct}=="0a6b", GROUP="corsair", TAG+="systemd", ENV{SYSTEMD_WANTS}="corsair-hs80-pipewire-thing.service"
+    SUBSYSTEM=="hidraw", ATTRS{idVendor}=="1b1c", ATTRS{idProduct}=="0a6b", GROUP="corsair"
   '';
 
-  systemd.services.corsair-hs80-pipewire-thing = {
+  systemd.user.services.corsair-hs80-pipewire-thing = {
     serviceConfig = {
       ExecStart = "${inputs.corsair-hs80-pipewire-thing.packages.${pkgs.stdenv.hostPlatform.system}.default}/bin/corsair-hs80-pipewire-thing";
-      User = "marie";
-      Group = "corsair";
     };
-    unitConfig = {
-      StopWhenUnneeded = true;
-    };
+    wantedBy = [ "graphical-session.target" ];
   };
 }
