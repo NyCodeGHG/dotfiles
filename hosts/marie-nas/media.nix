c4cbcb78008d72c8d49b786cbb6930bf710d0b3a
Marie Ramlow <me@nycode.dev>
2025-04-12 19:49:17 +0200

    marie-nas/jellyfin: open firewall

--- hosts/marie-nas/media.nix
+++ hosts/marie-nas/media.nix
@@ -3,6 +3,7 @@
   services.jellyfin = {
     enable = true;
     group = "media";
+    openFirewall = true;
   };
   services.nginx.virtualHosts."jellyfin.marie.cologne".locations."/" = {
     proxyPass = "http://127.0.0.1:8096";
