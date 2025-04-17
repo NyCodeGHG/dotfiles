ea9c447f6fde1e7b303f0ed29536acf94c5f31f5
Marie Ramlow <me@nycode.dev>
2025-04-17 13:26:17 +0200

    flake: switch to 24.11

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11-small";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable-small";
 
     home-manager-unstable = {
