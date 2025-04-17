b9336c0b936b6b2c4d0fb4a3a28a3cccdf7a655b
Marie Ramlow <me@nycode.dev>
2025-04-17 12:36:12 +0200

    vendor go 1.24

--- flake.nix
+++ flake.nix
@@ -138,6 +138,10 @@
         jellyfin-plugin-sso = prev.callPackage ./pkgs/jellyfin-plugin-sso/package.nix { };
         bitmagnet = prev.callPackage ./pkgs/bitmagnet/package.nix { };
         jellyseerr = prev.callPackage ./pkgs/jellyseerr/package.nix { };
+        go_1_24 = prev.callPackage ./pkgs/go_1_24/package.nix { };
+        buildGo124Module = prev.buildGoModule.override {
+          go = final.go_1_24;
+        };
       });
 
       nixosModules = {
