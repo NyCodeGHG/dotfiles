a4941bddc37bf5083b54bc838f9f1d861a4b0894
Marie Ramlow <me@nycode.dev>
2025-05-19 19:24:56 +0200

    users/marie: add to adbusers

--- config/nixos/users/marie.nix
+++ config/nixos/users/marie.nix
@@ -6,7 +6,8 @@ lib.mkIf config.uwumarie.profiles.users.marie {
     extraGroups = [ "wheel" ] ++
       lib.optional config.programs.gamemode.enable "gamemode" ++
       lib.optional config.virtualisation.libvirtd.enable "libvirtd" ++
-      lib.optional config.services.pipewire.enable "pipewire";
+      lib.optional config.services.pipewire.enable "pipewire" ++
+      lib.optional config.programs.adb.enable "adbusers";
     openssh.authorizedKeys.keys = lib.mkIf config.services.openssh.enable [
       # Desktop old
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFiS+tzh0R/nN5nqSwvLerCV4nBwI51zOKahFfiiINGp"
