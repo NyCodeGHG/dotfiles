87150107ee15899d74204aecb091f1d25e65cdda
Marie Ramlow <me@nycode.dev>
2025-07-15 00:09:51 +0200

    add confirmation prompt to switch-to-windows

--- modules/hm/switch-to-windows.nix
+++ modules/hm/switch-to-windows.nix
@@ -4,12 +4,18 @@
   pkgs,
   ...
 }:
+let
+  rebootScript = pkgs.writeShellScriptBin "reboot-to-windows" ''
+    ${lib.getExe pkgs.kdePackages.kdialog} --title "Reboot into Windows?" --yesno "Are you sure you want to reboot into Windows?" \
+      && systemctl reboot --boot-loader-entry=auto-windows
+  '';
+in
 {
   options.programs.switch-to-windows.enable = lib.mkEnableOption "Reboot into windows desktop entry";
   config = lib.mkIf config.programs.switch-to-windows.enable {
     xdg.desktopEntries.switch-to-windows = {
       name = "Reboot to Windows";
-      exec = "systemctl reboot --boot-loader-entry=auto-windows";
+      exec = (lib.getExe rebootScript);
       terminal = false;
       categories = [ "System" ];
       icon = pkgs.fetchurl {
@@ -17,5 +23,6 @@
         hash = "sha256-D2t02Njt4ewORUs8b837pl++mJr5pJrU/wxJj03Dr7s=";
       };
     };
+    home.packages = [ rebootScript ];
   };
 }
