699b9c55d605e7af330d4e949cc959201eb000c2
Marie Ramlow <me@nycode.dev>
2025-07-28 03:09:52 +0200

    marie-nas/factorio: more config

--- hosts/marie-nas/services/factorio.nix
+++ hosts/marie-nas/services/factorio.nix
@@ -6,14 +6,17 @@
 {
   services.factorio = {
     enable = true;
+    bind = "[::]";
     package = pkgs.factorio-headless.overrideAttrs (prev: rec {
       version = "2.0.60";
 
       src = pkgs.fetchurl {
         url = "https://factorio.com/get-download/${version}/headless/linux64";
+        name = "factorio-headless-${version}.tar.xz";
         hash = "sha256-abW+GoZ/2ZUk+ZFN/ukAoaw4bPTnTEpjdowF3E0rKws=";
       };
 
+      # Remove Space Age
       installPhase = prev.installPhase + ''
         rm -rf $out/share/factorio/data/{quality,elevated-rails,space-age}
       '';
@@ -26,6 +29,15 @@
     allowedPlayers = [
       "uwumarie"
     ];
+    game-name = "meow";
+    description = "factoworio server";
+    loadLatestSave = true;
+    nonBlockingSaving = true;
+    autosave-interval = 15;
+    requireUserVerification = true;
+    extraSettings = {
+      autosave_slots = 15;
+    };
   };
 
   nixpkgs.config.allowUnfreePredicate =
