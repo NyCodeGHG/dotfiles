9ef7c322ad5dd38e9bd2b2b7bf8e486220b917e3
Marie Ramlow <me@nycode.dev>
2025-08-05 14:57:03 +0200

    artemis: upgrade to PostreSQL 17

--- hosts/artemis/postgres.nix
+++ hosts/artemis/postgres.nix
@@ -2,10 +2,6 @@
 {
   services.postgresql = {
     enable = true;
-    package = pkgs.postgresql_15;
-    settings = {
-      max_wal_senders = 3;
-      wal_level = "replica";
-    };
+    package = pkgs.postgresql_17;
   };
 }
