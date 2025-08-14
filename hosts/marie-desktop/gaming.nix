d466c4580598e91d453760365790d9fa810117eb
Marie Ramlow <me@nycode.dev>
2025-08-14 18:41:38 +0200

    marie-desktop: use openrgb with all plugins

--- hosts/marie-desktop/gaming.nix
+++ hosts/marie-desktop/gaming.nix
@@ -66,5 +66,8 @@
     openFirewall = true;
   };
 
-  services.hardware.openrgb.enable = true;
+  services.hardware = {
+    openrgb.enable = true;
+    openrgb.package = pkgs.openrgb-with-all-plugins;
+  };
 }
