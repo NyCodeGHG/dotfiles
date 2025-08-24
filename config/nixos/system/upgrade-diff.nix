83265c8f70219891733618013879c6f4ae0167d7
Marie Ramlow <me@nycode.dev>
2025-08-24 14:32:47 +0200

    upgrade-diff: switch to lix-diff

--- config/nixos/system/upgrade-diff.nix
+++ config/nixos/system/upgrade-diff.nix
@@ -1,12 +1,17 @@
 # MIT Jörg Thalheim - https://github.com/Mic92/dotfiles/blob/c6cad4e57016945c4816c8ec6f0a94daaa0c3203/nixos/modules/upgrade-diff.nix
-{ config, pkgs, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 {
   system.activationScripts.diff = {
     supportsDryActivation = true;
     text = ''
       if [[ -e /run/current-system ]]; then
         echo "--- diff to current-system"
-        ${pkgs.nvd}/bin/nvd --nix-bin-dir=${config.nix.package}/bin diff /run/current-system "$systemConfig"
+        ${lib.getExe pkgs.lix-diff} --lix-bin ${config.nix.package}/bin /run/current-system "$systemConfig"
         echo "---"
       fi
     '';
