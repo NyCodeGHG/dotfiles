52b6f3d4bfc6ba81bdb7a04910bd89f893cf895b
Marie Ramlow <me@nycode.dev>
2025-10-05 14:50:46 +0200

    artemis/grafana: migrate to kanidm

--- hosts/artemis/monitoring/grafana.nix
+++ hosts/artemis/monitoring/grafana.nix
@@ -26,7 +26,6 @@ in
         cookie_secure = true;
       };
       auth = {
-        signout_redirect_url = "https://sso.nycode.dev/if/session-end/grafana/";
         disable_login_form = true;
       };
       "auth.generic_oauth" = {
@@ -35,15 +34,15 @@ in
         enabled = true;
         allow_sign_up = true;
         auto_login = false;
-        client_id = "faec063568d698926dbb10fb4d3fcc59761c5a7d";
+        client_id = "grafana";
         client_secret = "$__file{${config.age.secrets.grafana-oauth-client-secret.path}}";
         scopes = "openid profile email";
-        auth_url = "https://sso.nycode.dev/application/o/authorize/";
-        token_url = "https://sso.nycode.dev/application/o/token/";
-        api_url = "https://sso.nycode.dev/application/o/userinfo/";
+        auth_url = "https://idm.marie.cologne/ui/oauth2";
+        token_url = "https://idm.marie.cologne/oauth2/token";
+        api_url = "https://idm.marie.cologne/oauth2/openid/grafana/userinfo";
         use_pkce = true;
         allow_assign_grafana_admin = true;
-        role_attribute_path = "contains(groups[*], 'grafana-server-admin') && 'GrafanaAdmin' || contains(groups[*], 'grafana-admin') && 'Admin' || contains(groups[*], 'grafana-edit') && 'Editor' || 'Viewer'";
+        role_attribute_path = "contains(grafana_roles[*], 'grafana-server-admin') && 'GrafanaAdmin' || contains(grafana_roles[*], 'grafana-admin') && 'Admin' || contains(grafana_roles[*], 'grafana-edit') && 'Editor' || 'Viewer'";
         login_attribute_path = "preferred_username";
         name_attribute_path = "nickname";
         id_token_attribute_name = "sub";
