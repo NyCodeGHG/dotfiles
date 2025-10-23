8720df7b84fd696da9c86bf7093ed73a4f8f5566
Marie Ramlow <me@nycode.dev>
2025-10-23 20:16:10 +0200

    gitlabber/forgejo-runner: add host label

--- hosts/gitlabber/forgejo-runner.nix
+++ hosts/gitlabber/forgejo-runner.nix
@@ -8,6 +8,7 @@
         name = "gitlabber-01";
         labels = [
           "ubuntu-22.04:docker://ghcr.io/catthehacker/ubuntu:runner-22.04"
+          "nix-x86_64:host"
         ];
         tokenFile = config.age.secrets.forgejo-runner.path;
         url = "https://git.marie.cologne";
