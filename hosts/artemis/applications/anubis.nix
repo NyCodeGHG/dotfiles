8dd419ca8a9a20f209ccd247dc0708adc8e91293
Marie Ramlow <me@nycode.dev>
2025-11-26 22:57:12 +0100

    artemis/anubis: add policy file

--- hosts/artemis/applications/anubis.nix
+++ hosts/artemis/applications/anubis.nix
@@ -1,7 +1,25 @@
 {
   config,
+  pkgs,
   ...
 }:
+let
+  configFile = (pkgs.formats.yaml { }).generate "anubis-policy-config.yaml" {
+    bots = [
+      { import = "(data)/meta/default-config.yaml"; }
+      {
+        name = "forgejo-runner";
+        path_regex = "^/api/actions/(?:runner.v1.RunnerService|ping.v1.PingService)/.*$";
+        action = "ALLOW";
+      }
+      {
+        name = "blackbox-exporter";
+        user_agent_regex = "^Blackbox Exporter\/\d+\.\d+\.\d+$";
+        action = "ALLOW";
+      }
+    ];
+  };
+in
 {
   users.users.nginx.extraGroups = [
     config.users.groups.anubis.name
@@ -9,5 +27,6 @@
 
   services.anubis.defaultOptions.settings = {
     SERVE_ROBOTS_TXT = true;
+    POLICY_FNAME = toString configFile;
   };
 }
