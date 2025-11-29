814c96d5284892377b37210254aac9e31fee9334
Marie Ramlow <me@nycode.dev>
2025-11-29 13:59:49 +0100

    artemis/paperless: remove tika and gotenberg

--- hosts/artemis/applications/paperless.nix
+++ hosts/artemis/applications/paperless.nix
@@ -6,9 +6,6 @@
       PAPERLESS_URL = "https://paperless.marie.cologne";
       PAPERLESS_TRUSTED_PROXIES = "127.0.0.1";
       PAPERLESS_OCR_LANGUAGE = "deu+eng";
-      PAPERLESS_TIKA_ENABLED = true;
-      PAPERLESS_TIKA_ENDPOINT = "http://127.0.0.1:${toString config.services.tika.port}";
-      PAPERLESS_TIKA_GOTENBERG_ENDPOINT = "http://127.0.0.1:${toString config.services.gotenberg.port}";
       PAPERLESS_ENABLE_COMPRESSION = false;
       PAPERLESS_USE_X_FORWARD_HOST = true;
       PAPERLESS_USE_X_FORWARD_PORT = true;
@@ -52,12 +49,5 @@
   services.nginx.tailscaleAuth = {
     enable = true;
     virtualHosts = [ "paperless.marie.cologne" ];
-  };
-
-  services.tika.enable = true;
-  services.gotenberg = {
-    enable = true;
-    chromium.disableJavascript = true;
-    port = 26840;
   };
 }
