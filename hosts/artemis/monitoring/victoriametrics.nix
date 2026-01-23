39cdbcdd2006f364d8a5aed42a2499c201283c77
Marie Ramlow <me@nycode.dev>
2026-01-23 21:53:12 +0100

    artemis/victoriametrics: scrape untis-caldav-sync

--- hosts/artemis/monitoring/victoriametrics.nix
+++ hosts/artemis/monitoring/victoriametrics.nix
@@ -3,6 +3,24 @@
   services.victoriametrics = {
     enable = true;
     retentionPeriod = "30d";
+    prometheusConfig = {
+      global = {
+        scrape_interval = "30s";
+        scrape_timeout = "10s";
+      };
+      scrape_configs = [
+        {
+          job_name = "untis-caldav-sync";
+          metrics_path = "/metrics";
+          static_configs = [
+            {
+              targets = [ "localhost:3002" ];
+              labels.environment = "staging";
+            }
+          ];
+        }
+      ];
+    };
   };
 
   services.nginx.tailscaleAuth = {
