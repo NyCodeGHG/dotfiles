4b1a78ae6e9439bc287135dbc767bd85a0eb0db5
Marie Ramlow <me@nycode.dev>
2026-03-03 11:31:46 +0100

    profiles/headless: don't disable fonts.fontconfig.enable

--- config/nixos/system/headless.nix
+++ config/nixos/system/headless.nix
@@ -2,7 +2,6 @@
 {
   options.uwumarie.profiles.headless = lib.mkEnableOption "headless profile";
   config = lib.mkIf config.uwumarie.profiles.headless {
-    fonts.fontconfig.enable = false;
     environment.stub-ld.enable = false;
     networking.firewall.logRefusedConnections = false;
     powerManagement.enable = lib.mkDefault false;
