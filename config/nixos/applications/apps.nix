2d96f2fb5f32739613bd48b2e31315c2a860ccfb
Marie Ramlow <me@nycode.dev>
2026-04-11 17:44:38 +0200

    add distrobox fix

--- config/nixos/applications/apps.nix
+++ config/nixos/applications/apps.nix
@@ -109,5 +109,9 @@
 
     programs.firefox.enable = true;
     programs.thunderbird.enable = true;
+
+    environment.etc."distrobox/distrobox.conf".text = ''
+      container_additional_volumes="/nix/store:/nix/store:ro /etc/profiles/per-user:/etc/profiles/per-user:ro /etc/static/profiles/per-user:/etc/static/profiles/per-user:ro"
+    '';
   };
 }
