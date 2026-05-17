5b85ced46c5b66e1f1fbe1e5d9dd81c4e29a5828
Marie Ramlow <me@nycode.dev>
2026-05-17 17:48:20 +0200

    marie-nas: remove Type=notify for transmission

--- hosts/marie-nas/services/transmission.nix
+++ hosts/marie-nas/services/transmission.nix
@@ -10,7 +10,6 @@
     bindsTo = [ "netns@vpn.target" ];
     serviceConfig = {
       NetworkNamespacePath = "/var/run/netns/vpn";
-      Type = "notify";
       BindReadOnlyPaths = "${config.vpn.dns.resolvconf}:/etc/resolv.conf:norbind";
       InaccessiblePaths = "/run/nscd/socket";
     };
