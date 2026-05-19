3ac0e0bf00714f8f4ab3089bc50bf5cfc8bdd29c
Marie Ramlow <me@nycode.dev>
2026-05-19 12:02:51 +0200

    proxyt: sync with nixpkgs

--- pkgs/proxyt/package.nix
+++ pkgs/proxyt/package.nix
@@ -1,7 +1,9 @@
 {
   lib,
+  stdenv,
   buildGoModule,
   fetchFromGitHub,
+  installShellFiles,
 }:
 
 buildGoModule (finalAttrs: {
@@ -18,14 +20,29 @@ buildGoModule (finalAttrs: {
   vendorHash = "sha256-wdbfcYS3A0c/XDnMxyBOx4pFdMk3FOYQlwTmbS/zMj4=";
 
   ldflags = [
+    "-s"
+    "-w"
     "-X=github.com/jaxxstorm/proxyt/cmd.Version=${finalAttrs.version}"
   ];
 
+  env = {
+    CGO_ENABLED = "0";
+  };
+
+  nativeBuildInputs = [ installShellFiles ];
+
+  postInstall = lib.optionalString (stdenv.buildPlatform.canExecute stdenv.hostPlatform) ''
+    installShellCompletion --cmd proxyt \
+      --bash <($out/bin/proxyt completion bash) \
+      --fish <($out/bin/proxyt completion fish) \
+      --zsh <($out/bin/proxyt completion zsh)
+  '';
+
   meta = {
-    description = "";
+    description = "Simple proxy for the Tailscale control plane";
     homepage = "https://github.com/jaxxstorm/proxyt";
     license = lib.licenses.mit;
-    maintainers = with lib.maintainers; [ ];
+    maintainers = with lib.maintainers; [ marie ];
     mainProgram = "proxyt";
   };
 })
