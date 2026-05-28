096ad11712ec7b25c275d42771254477f371b8aa
Marie Ramlow <me@nycode.dev>
2026-05-28 22:38:48 +0200

    also pick importCargoLock PR on unstable

--- flake.nix
+++ flake.nix
@@ -212,6 +212,7 @@
                 nixpkgs-unstable = [
                   # ./patches/nixpkgs-kapsule.patch
                   (npr 524144 "sha256-vJ5So/nbSga0Lk8TqKFODgmxmxuVyIIgkHSo6DK6BA0=")
+                  (npr 524985 "sha256-Ir57/CI5fmGxmvlhhsdHLMpb53zm/jzYvOVWQrpkAhw=")
                 ];
                 nixpkgs = [
                   (npr 524985 "sha256-Ir57/CI5fmGxmvlhhsdHLMpb53zm/jzYvOVWQrpkAhw=")
