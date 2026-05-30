3a536ed139f24a88b065ba951834d965fd09a1dd
Marie Ramlow <me@nycode.dev>
2026-05-30 16:56:24 +0200

    marie-nas/otbr: set logLevel to warning

--- hosts/marie-nas/services/smart-home/openthread-border-router.nix
+++ hosts/marie-nas/services/smart-home/openthread-border-router.nix
@@ -3,7 +3,7 @@
   services.openthread-border-router = {
     enable = true;
     backboneInterfaces = [ "br0" ];
-    logLevel = "info";
+    logLevel = "warning";
     radio = {
       device = "/dev/serial/by-id/usb-Itead_Sonoff_Zigbee_3.0_USB_Dongle_Plus_V2_b2755e278739ef11a42a53f454516304-if00-port0";
       baudRate = 460800;
