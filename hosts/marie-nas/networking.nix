08512a1b08cbf811baab829debdce78663bd909c
Marie Ramlow <me@nycode.dev>
2026-06-21 20:55:03 +0200

    marie-nas: add talos as trusted interface

--- hosts/marie-nas/networking.nix
+++ hosts/marie-nas/networking.nix
@@ -103,5 +103,5 @@
     enable = true;
     settings.Resolve.MulticastDNS = "resolve";
   };
-  networking.firewall.trustedInterfaces = [ "podman*" ];
+  networking.firewall.trustedInterfaces = [ "podman*" "talos" ];
 }
