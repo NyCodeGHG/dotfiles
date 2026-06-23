c529119c4bfe2963b7910cd4e2c5e455f13f250c
Marie Ramlow <me@nycode.dev>
2026-06-23 13:18:02 +0200

    marie-nas/openthread-border-router: lower baud rate

--- hosts/marie-nas/services/smart-home/openthread-border-router.nix
+++ hosts/marie-nas/services/smart-home/openthread-border-router.nix
@@ -6,7 +6,7 @@
     logLevel = "warning";
     radio = {
       device = "/dev/serial/by-id/usb-Itead_Sonoff_Zigbee_3.0_USB_Dongle_Plus_V2_b2755e278739ef11a42a53f454516304-if00-port0";
-      baudRate = 460800;
+      baudRate = 115200;
       flowControl = false;
     };
     web.enable = true;
