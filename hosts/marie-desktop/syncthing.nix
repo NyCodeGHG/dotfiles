4f672c0f8a271f4180b802b9d97ebb3d25b5ced3
Marie Ramlow <me@nycode.dev>
2026-06-23 13:18:02 +0200

    {marie-desktop,steamdeck}: syncthing prismlauncher

--- hosts/marie-desktop/syncthing.nix
+++ hosts/marie-desktop/syncthing.nix
@@ -8,5 +8,29 @@
     openDefaultPorts = true;
     overrideDevices = false;
     overrideFolders = false;
+    settings = {
+      devices = {
+        steamdeck.id = "ELXO6JB-KGVO56W-EKNX3I5-Z6YHMOV-4AK26X6-7EJ66EG-BKOMBMU-U3CK2QB";
+      };
+      folders = {
+        prismlauncher = {
+          id = "prismlauncher";
+          devices = [ "steamdeck" ];
+          label = "PrismLauncher";
+          path = "/home/marie/.local/share/PrismLauncher/";
+          ignorePatterns = [
+            "/accounts.json"
+            "/metacache"
+            "/images"
+            "/assets"
+            "/libraries"
+            "/meta"
+            "/cache"
+            "/logs"
+            "/translations"
+          ];
+        };
+      };
+    };
   };
 }
--- hosts/steamdeck/syncthing.nix
+++ hosts/steamdeck/syncthing.nix
@@ -6,15 +6,34 @@
     user = "marie";
     group = "users";
     openDefaultPorts = true;
-    devices = {
-      marie-desktop.id = "6TPYJWH-7QOBNQI-RCUCWBM-SYKPS6M-XOS3YXB-4LFTLBE-BCPOJ6L-ET6PJAG";
-    };
-    folders = {
-      satisfactory-saves = {
-        id = "juy6j-mmhjp";
-        devices = [ "marie-desktop" ];
-        label = "Satisfactory Saves";
-        path = "/home/marie/Games/Heroic/Prefixes/Satisfactory/pfx/drive_c/users/steamuser/AppData/Local/FactoryGame/Saved/SaveGames/";
+    settings = {
+      devices = {
+        marie-desktop.id = "6TPYJWH-7QOBNQI-RCUCWBM-SYKPS6M-XOS3YXB-4LFTLBE-BCPOJ6L-ET6PJAG";
+      };
+      folders = {
+        satisfactory-saves = {
+          id = "juy6j-mmhjp";
+          devices = [ "marie-desktop" ];
+          label = "Satisfactory Saves";
+          path = "/home/marie/Games/Heroic/Prefixes/Satisfactory/pfx/drive_c/users/steamuser/AppData/Local/FactoryGame/Saved/SaveGames/";
+        };
+        prismlauncher = {
+          id = "prismlauncher";
+          devices = [ "marie-desktop" ];
+          label = "PrismLauncher";
+          path = "/home/marie/.local/share/PrismLauncher/";
+          ignorePatterns = [
+            "/accounts.json"
+            "/metacache"
+            "/images"
+            "/assets"
+            "/libraries"
+            "/meta"
+            "/cache"
+            "/logs"
+            "/translations"
+          ];
+        };
       };
     };
   };
