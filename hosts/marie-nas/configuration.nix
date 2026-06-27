592c0444faea885682968b5cdaeb601e6d29155d
Marie Ramlow <me@nycode.dev>
2026-06-27 21:27:34 +0200

    marie-nas: comment out OTBR

--- hosts/marie-nas/configuration.nix
+++ hosts/marie-nas/configuration.nix
@@ -32,7 +32,7 @@
     ./services/smart-home/matter-server.nix
     ./services/smart-home/mosquitto.nix
     ./services/smart-home/music-assistant.nix
-    ./services/smart-home/openthread-border-router.nix
+    # ./services/smart-home/openthread-border-router.nix
     ./services/smart-home/zigbee2mqtt.nix
 
     # Media
