78e14fa175df99da2b171d3b4ec5249392c2cd49
Marie Ramlow <me@nycode.dev>
2026-08-19 10:24:20 +0200

    flake: switch to channels.nixos.org

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.zst";
 
     home-manager = {
       url = "github:nix-community/home-manager";
